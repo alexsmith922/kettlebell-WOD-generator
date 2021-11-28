b2f569dd21b6640e8d27f53b6d489d003d867a5f
alexsmith922 <54478329+alexsmith922@users.noreply.github.com>
2021-11-28 10:23:43 -0600

    Added files via upload
    Initial commit

---
+++ WOD.py
@@ -0,0 +1,6 @@
+import random
+from workouts import workouts
+workout_number = random.randint(0,25)
+
+print(workouts[workout_number])
+
---
+++ workouts/__init__.py
@@ -0,0 +1,223 @@
+workouts = \
+    [
+        """The Perfect Pair
+Double Handed Swing – 20 reps
+Push Ups – 10, 9, 8, 7 etc.
+Repeat – 10 times""",
+        """Take Five Kettlebell Circuit Workout
+Double Handed Squats – 20 reps
+Snatch – 20 reps
+Reverse Lunge – 20 reps
+Push Up – 20 reps
+Double Handed Swing – 20 reps
+Repeat – 15, 10, 5 reps""",
+        """Climb to Fitness with Kettle Bells
+Burpees – 30 secs / rest 30 secs
+Slow Mountain Climbers – 30 secs
+Alternating Swings – 30 secs
+Push Ups – 30 secs
+Double Handed Squats – 30 secs
+Sit & Press – 30 secs
+Double Handed Swings – 30 secs
+Fast Mountain Climbers – 30 secs
+Repeat – add another exercise each round""",
+        """The Kettle bell Workout Test
+Snatch – 3 mins
+Push Ups – 3 mins
+Double Handed Squats – 3 mins
+Slow Mountain Climbers – 3 mins
+Double Lunge – 3 mins
+Rest 60 seconds after each exercise""",
+        """5 Step Kettle bell Workout Ladder
+One Handed Swing – 5 reps each
+High Pulls – 5 reps each
+Snatch – 5 reps each
+Windmill – 5 reps each
+Repeat – adding 1 more exercise below each round
+Clean & Press – 5 reps each
+Double Lunge – 5 reps each
+Squat & Press – 5 reps each""",
+        """Kettle bell Workout Super Sized
+Clean & Press – 10 reps each
+Double Lunge – 10 reps each
+Single Leg Deadlift – 10 reps each
+Squat Racked – 10 reps each
+Windmill – 10 reps each
+Burpees – 20 reps
+Fast Mountain Climbers – 20 reps
+Jump Squats – 20 reps
+Double Handed Swings – 20 reps
+High Pulls – 20 reps each""",
+        """KB Workout Super Sized V.2
+Windmill – 10 reps each
+Double Lunge – 10 reps each
+Snatch – 10 reps each
+Thruster – 10 reps each
+T – Push Ups – 10 reps each
+Double Handed Swings – 20 reps
+No Kettlebell Squats – 20 reps
+High Pulls – 20 reps each
+Bob & Weave – 20 reps
+Alternating Reverse Lunges (no bell) – 20 reps""",
+        """A Pressing Minute and Kettlebells
+One Handed Swing – 60 secs each
+Double Lunge – 60 secs each
+High Pulls – 60 secs each
+Squat & Press – 60 secs each
+Snatch – 60 secs each
+Sit & Press – 60 secs
+Rest 60 seconds between exercises + 10 Push Ups""",
+        """1 Minute Kettle bell Workout Intervals
+One Handed Swing – 60 secs each
+Reverse Lunge Overhead – 60 secs each
+Clean – 60 secs each
+Squat & Press – 60 secs each
+Snatch – 60 secs each
+Clean, Squat & Press – 60 secs each
+Rest 60 seconds between exercises""",
+        """1 Minute Kettlebell Workout Circuits
+Double Handed Squat – 60 secs
+Slow Mountain Climbers – 60 secs
+Alternating Swing – 60 secs
+Push Ups – 60 secs
+Jumping Lunges – 60 secs
+Dirty Dogs – 60 secs
+Bob & Weave – 60 secs
+High Pulls – 60 secs each
+Double Lunge – 60 secs each
+Squat & Press – 60 secs each
+Rest 15 seconds after each exercise""",
+        """A Burping Kettle bell Workout Minute
+One Handed Swing – 60 secs each
+Reverse Lunge – 60 secs each
+Squat & Press – 60 secs each
+Clean – 60 secs each
+Push Ups – 60 secs
+Sit & Press – 60 secs
+Rest 60 seconds between exercises + 10 Burpees""",
+        """300 Classic Kettlebell Workout Challenge
+One Handed Swing – 50 reps each
+Snatch – 25 reps each
+Clean & Press – 25 reps each
+Reverse Lunge – 25 reps each
+Squat & Press – 25 reps each""",
+        """300 Kettlebell Workout Challenge V.2.
+Snatch – 25 reps each
+Squat & Press – 25 reps each
+High Pulls – 25 reps each
+Bob & Weave – 50 reps
+One Handed Swing – 50 reps each""",
+        """2 x 7 minutes KB Workout Circuits
+Double Lunge – 5 reps each
+Squat & Press – 5 reps each
+Snatch – 5 reps each
+Double Handed Swings – 20 reps
+Burpees – 10 reps
+Fast Mountain Climbers – 20 reps
+Perform each circuit for 7 minutes""",
+        """2 x 7 minutes V.2 Kettlebell Circuits
+Single Leg Deadlift – 5 reps each
+Side Lunge – 5 reps each
+T – Push Up – 5 reps each
+Two Handed Handed Swings – 20 reps
+Bodyweight Reverse Lunges – 20 reps
+Bodyweight Squats – 20 reps
+Perform each circuit for 7 minutes""",
+        """2 x 7 minutes V.3 KB Workout Circuits
+Clean, Squat & Press – 5 reps each
+Double Lunge – 5 reps each
+T – Push Ups – 5 reps each
+Burpees – 10 reps
+Jumping Lunges – 20 reps
+Two Handed Swings – 20 reps
+Perform each circuit for 7 minutes""",
+        """10 and Walk Kettle bell Workout
+One Handed Swing – 10 reps each x 2
+High Pulls – 10 reps each x 2
+Snatch – 10 reps each x 2
+Clean, Squat & Press – 10 reps each x 2
+Walking Lunge – 10 reps between each exercise""",
+        """30 Second Kettlebell Supersets
+One Handed Swing – 30 secs each
+Squat & Press – 30 secs each
+High Pulls – 30 secs each
+Double Lunges – 30 secs each
+Perform each pair 3 times""",
+        """Double Trouble (2 Kettlebells Required)
+Double Squat – 1 min
+Push Ups – 1 min
+Burpees – 1 min
+Reverse Lunge – 1 min
+Clean & Press – 1 min
+Bodyweight Bob & Weave – 1 min
+Perform each pair 3 times""",
+        """50/10 Kettlebell Routine
+Snatch – 50 secs each
+Burpee – 50 secs
+Clean & Press – 50 secs each
+Jump Squats – 50 secs
+Windmill – 50 secs each
+Lunge & Hop – 50 secs each
+Push Ups – 50 secs
+High Pulls – 50 secs each
+Squat & Hold – 50 secs
+Slow Mountain Climbers – 50 secs
+One Handed Swings – 50 secs each
+Sit & Press – 50 secs each
+Rest 10 secs between exercises""",
+        """Beat the Cards KB Workout Challenge
+Hearts – Push Ups
+Spades – Reverse Lunge
+Clubs – Slow Mountain Climbers
+Diamonds – Squat & Press
+Jack – High Pulls x 10 each
+Queen – Snatch x 10 each
+King – One Handed Swings x 10 each
+Ace – Reverse Turkish Get Up x 1 each
+Joker (optional) – 1 Minute Rest""",
+        """3 x Classic Kettlebell Workout Circuits
+One Handed Swing – 30 secs each
+Clean & Press – 30 secs each
+Side Lunge – 30 secs each
+Clean & Squat – 30 secs each
+Alternating Swing – 30 secs each
+Double Lunge – 30 secs each
+Squat & Press – 30 secs each
+Windmill – 30 secs each
+Double Handed Swing – 60 secs
+Clean, Squat & Press – 30 secs each
+High Pulls – 30 secs each
+Overhead Forward Lunge – 30 secs each
+Rest 60 secs between circuits""",
+        """Big Circuit Kettlebell Routine
+Snatch – 60 secs each
+Burpee – 60 secs
+Push Ups – 60 secs
+Alternating Swing – 60 secs
+Squat Thrusts – 60 secs
+Overhead Press – 60 secs each
+Rest 1-2 mins repeat max 3 times""",
+        """Tabata Time Kettlebell Routine
+Push Ups – 20 secs
+Alternating Swing – 20 secs
+Fast Mountain Climbers – 20 secs
+High Pulls – 20 secs
+Snatch – 20 secs
+Jumping Lunge – 20 secs
+Rest 10 secs after each exercise
+Repeat each pair 8 times""",
+        """10 then 5 Kettlebell Workout Routine
+One Handed Swing – 10 reps each
+High Pulls – 10 reps each
+Snatch – 10 reps each
+Rest 60 secs
+Squat & Press – 10 reps each
+Alternating Swing – 10 reps each
+Clean & Press – 10 reps each
+Rest 60 secs
+Side Lunge – 10 reps each
+Two Handed Swing – 20 reps
+Reverse Lunge & Press – 10 reps each
+Rest 60 secs
+Repeat for 5 reps each side with no rests"""
+    ]
